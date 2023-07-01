2ad89a49e03682ff99849fe281cda929da675d36
Andrii S <andriisharapatov@gmail.com>
2023-07-02 02:05:53 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "in3ZM1FfOyIlc5-1lIOuUEfi4i_vBJACVINSRLdsLh8",
+      "spaceId": "k9kptl22xzvk"
+    }
+  }, "gatsby-plugin-image", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "387120216"
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
